0a14b147a4cda9d8ebc876aa923d5d171a0fa348
Maja-co <onlineshit61@gmail.com>
2024-09-20 08:35:50 +0200

    lol

--- src/opgave03/Pigs.java
+++ src/opgave03/Pigs.java
@@ -1,4 +1,15 @@
 package opgave03;
 
+import java.util.Scanner;
+
 public class Pigs {
+    public static void main(String[] args) {
+        playPig();
+    }
+
+    public static void playPig() {
+        Scanner pig = new Scanner(System.in);
+
+    }
 }
+
